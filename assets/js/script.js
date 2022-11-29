b950e49f65bb34942fc7b9928afa12a39915e074
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-11-28 19:49:51 -0500

    user input and isplay in search history

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Collect User Input and Display in Search History
+function addResult(){
+    inputCity = document.getElementById("myInput").value;  
+    historyList = getInfo();
+    var searchCity =$("<div>") 
+    searchCity.attr('id',inputCity) 
+    searchCity.text(inputCity) 
+    searchCity.addClass("h4")
+
+    if (historyList.includes(inputCity) === false){
+        $(".history").append(searchCity)
+    }
+    $(".subtitle").attr("style","display:inline")
+    addInfo(inputCity);   
+}; 
