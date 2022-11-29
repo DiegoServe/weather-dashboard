9bf3e96f915fc2d4c77d4a18b0f9180f8554f954
DiegoServe <110145883+DiegoServe@users.noreply.github.com>
2022-11-29 08:55:05 -0500

    init page and api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,17 @@
-let weather = {
-    "apiKey": "490ad53367baba09e51242fc3ea97ebb",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + this.apiKey)
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        // target each of the elements
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+    const APIKey = "aa66bff299d610638630deec7feb874a"
 
-        // display each element on the featured city block
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°F";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
-        document.querySelector(".weather").classList.remove("hidden");
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    }
-};
-
-document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
-});
-
-// allow enter to excute the search
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-        weather.search();
-    }
-});
-
-
-
-
-
-// var getCityWeather = function(city) {
-//     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + "baltimore" + "&appid=490ad53367baba09e51242fc3ea97ebb";
-
-//     fetch(apiUrl).then(function(response) {
-//         response.json().then(function(data) {
-//           console.log(data);
-//         });
-//       });
-//     };
+   
